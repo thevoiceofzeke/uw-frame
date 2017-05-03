20734f72832d7d5cf5055797b5d6d67af674a1d1
zeke-witter <david.witter@wisc.edu>
2017-05-03 14:01:37 -0500

    Add widget-creator to frame

--- uw-frame-components/portal/widgets/routes.js
+++ uw-frame-components/portal/widgets/routes.js
@@ -1,8 +1,11 @@
 define(['require'], function(require) {
 
   return {
-    'demoWidgets' : {
-      templateUrl: require.toUrl('./partials/demo-widgets.html')
-    }
+    'demoWidgets': {
+      templateUrl: require.toUrl('./partials/demo-widgets.html'),
+    },
+    'widgetCreator': {
+      templateUrl: require.toUrl('./partials/widget-creator.html'),
+    },
   }
 });
