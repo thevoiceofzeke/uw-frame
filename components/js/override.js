c218003ae1cc65ac131d8ead83ebfeb6d970d956
David Witter <david.witter@wisc.edu>
2018-07-24 12:55:46 -0500

    feat: use myuw-app-bar component

--- components/js/override.js
+++ components/js/override.js
@@ -21,7 +21,7 @@ define(['angular'], function(angular) {
   return angular.module('override', [])
     .constant('OVERRIDE', {
       'APP_OPTIONS': {
-        // 'appMenuTemplateURL': 'portal/misc/partials/example-menu.html',
+        'appMenuTemplateURL': 'portal/misc/partials/example-menu.html',
         // 'enablePushContentMenu': true,
       },
       'SERVICE_LOC': {
@@ -30,7 +30,7 @@ define(['angular'], function(angular) {
         // },
         // 'aboutURL': 'staticFeeds/about-frame.json',
         // 'aboutPageURL': 'staticFeeds/about-page.json'
-        // 'messagesURL': 'staticFeeds/sample-messages.json',
+        'messagesURL': 'staticFeeds/sample-messages.json',
       },
       'MESSAGES': {
         // 'notificationsPageURL': '/notifications',
